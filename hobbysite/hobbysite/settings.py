ae2cd00e5a75604b5ba1a0d3f092ab6191943c0a
Lawrence Paul Cado Marino <lawrencemarino18@gmail.com>
2025-05-12 12:26:28 +0800

    update setting.py to suit azure website

--- hobbysite/hobbysite/settings.py
+++ hobbysite/hobbysite/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = 'django-insecure-p#47_+(v7mznxbf4ayde!ou)$q_zd&j=k(7(12(ddq#2))n!g2
 DEBUG = True
 
 # Allow Azure host (update with actual app URL in production)
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['pleasework.azurewebsites.net', 'localhost']
 
 # Application definition
 INSTALLED_APPS = [
